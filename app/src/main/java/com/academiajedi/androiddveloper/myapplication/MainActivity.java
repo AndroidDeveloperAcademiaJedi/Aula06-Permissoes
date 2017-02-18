3f84152bdb7a22ecbbdfc8209174d1370c08e9ea
Alex Soares de Siqueira <alexsoaresdesiqueira@gmail.com>
2017-02-18 00:31:12 -0200

    first commit

---
+++ app/src/main/java/com/academiajedi/androiddveloper/myapplication/MainActivity.java
@@ -0,0 +1,58 @@
+package com.academiajedi.androiddveloper.myapplication;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 0);
+                } else {
+                    realizarLigacao();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            realizarLigacao();
+        }
+    }
+
+    private void realizarLigacao(){
+
+        Uri uri = Uri.parse("tel:998396271");
+        Intent intent = new Intent(Intent.ACTION_CALL, uri);
+
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Toast.makeText(MainActivity.this, "Ação nao suportada pelo device", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
